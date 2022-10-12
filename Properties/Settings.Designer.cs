86c950a6e2528d191951d677f7c9a7a4e3d093ea
MichiganKnight <88414825+MichiganKnight@users.noreply.github.com>
2022-10-11 19:51:59 -0500

    Update Settings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -56,5 +56,17 @@ namespace Cookbook_Database.Properties {
                 this["RecipeType"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool IsImageVisible {
+            get {
+                return ((bool)(this["IsImageVisible"]));
+            }
+            set {
+                this["IsImageVisible"] = value;
+            }
+        }
     }
 }
