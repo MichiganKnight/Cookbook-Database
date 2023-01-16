6939aa873333b315e90e3ddb0096691ff8a9bc2d
MichiganKnight <88414825+MichiganKnight@users.noreply.github.com>
2023-01-16 00:27:40 -0600

    Update CooksCountryRecipeView

--- Windows/CooksCountryRecipeView.xaml.cs
+++ Windows/CooksCountryRecipeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Cookbook_Database.Properties;
+﻿using Cookbook_Database.DatabaseHandler;
+using Cookbook_Database.Properties;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,7 +21,35 @@ namespace Cookbook_Database.Windows
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            CreateLabels(Settings.Default.CooksCountryRecipeToDisplay);
+            foreach (string name in NameModel.NameModelToString())
+            {
+                CreateLabels(name);
+            }
+
+            foreach (string serving in ServingsModel.ServingsModelToString())
+            {
+                CreateLabels($"Serves {serving}");
+            }
+
+            foreach (string description in DescriptionModel.DescriptionModelToString())
+            {
+                CreateTextBlocks(description);
+            }
+
+            foreach (string quantity in QuantityModel.QuantityModelToString())
+            {
+                CreateTextBlocks(quantity);
+            }
+
+            foreach (string ingredient in IngredientNameModel.IngredientModelToString())
+            {
+                CreateTextBlocks(ingredient);
+            }
+
+            /*for (int i = 0; i < QuantityModel.QuantityModelToString().Count; i++)
+            {
+                CreateTextBlocks($"Step {i}:");
+            }*/
         }
 
         private void CreateLabels(string text)
@@ -41,6 +70,28 @@ namespace Cookbook_Database.Windows
             };
 
             RecipePanel.Children.Add(label);
+        }
+
+        private void CreateTextBlocks(string text)
+        {
+            string name = Regex.Replace($"{text}TextBox", @"[^a-zA-Z0-9]+", "");
+
+            TextBlock textBlock = new()
+            {
+                Text = text.Replace("TextBox", ""),
+                FontSize = 25,
+                FontWeight = FontWeights.Medium,
+                Background = null,
+                Foreground = Brushes.Black,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                TextAlignment = TextAlignment.Center,
+                Name = ReplaceWithWord(name),
+                Margin = new Thickness(10, 0, 0, 0),
+                TextWrapping = TextWrapping.Wrap,
+                Width = 500
+            };
+
+            RecipePanel.Children.Add(textBlock);
         }
 
         private void GoBackButton_Click(object sender, RoutedEventArgs e)
