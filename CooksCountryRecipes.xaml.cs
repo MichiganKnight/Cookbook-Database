ab8971354ee1f8d1e3e5497fd755c7086df59500
MichiganKnight <88414825+MichiganKnight@users.noreply.github.com>
2023-01-19 16:43:00 -0600

    Update CooksCountryRecipes.xaml.cs

--- CooksCountryRecipes.xaml.cs
+++ CooksCountryRecipes.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Cookbook_Database.DatabaseHandler;
 using Cookbook_Database.Properties;
-using Cookbook_Database.Windows;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -25,7 +23,7 @@ namespace Cookbook_Database
         {
             foreach (string name in NameModel.NameModelToString())
             {
-                CreateRecipeButtons(name);
+                CreateRecipeButtons(name, RecipePanel, Frame, false);
             }
         }
 
@@ -60,48 +58,6 @@ namespace Cookbook_Database
             IssuePanel.Visibility = Visibility.Collapsed;
 
             DisplayRecipeNames();
-        }
-
-        private void CreateRecipeButtons(string recipe)
-        {
-            //Settings.Default.PreviousPageInfo = Settings.Default.CooksCountryIssue;
-
-            string recipeName = Regex.Replace($"{recipe}Button", @"[^a-zA-Z0-9]+", "");
-
-            Button recipeButton = new()
-            {
-                Content = recipe,
-                Cursor = Cursors.Hand,
-                FontSize = 25,
-                FontWeight = FontWeights.Medium,
-                Background = null,
-                Foreground = Brushes.Blue,
-                BorderBrush = Brushes.LightGray,
-                Height = 50,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Name = ReplaceWithWord(recipeName),
-                Margin = new Thickness(10, 0, 0, 0)
-            };
-
-            RecipePanel.Children.Add(recipeButton);
-
-            recipeButton.MouseEnter += (s, e) =>
-            {
-                recipeButton.Background = Brushes.LightGray;
-            };
-
-            recipeButton.MouseLeave += (s, e) =>
-            {
-                recipeButton.Background = Brushes.White;
-            };
-
-            recipeButton.Click += (s, e) =>
-            {
-                Settings.Default.CooksCountryRecipeToDisplay = recipeButton.Content.ToString();
-
-                Frame.Visibility = Visibility.Visible;
-                Frame.NavigationService.Navigate(new CooksCountryRecipeView());
-            };
         }
 
         private void Button_2019_Click(object sender, RoutedEventArgs e)
